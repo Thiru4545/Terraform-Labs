53d3c6ab132ce19fd9052a822560e5d996c34f45
Boopalan.G <thirubalan3599@gmail.com>
2025-06-04 04:05:50 +0530

    Delete Day01 directory

--- Day01/Provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  access_key = "AKIAZ24ISOK6LAFT324F"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
-}
--- Day01/main.tf
+++
@@ -1,9 +0,0 @@
-## Provider's Example
-
-# Configure the AWS Provider
-provider "aws" {
-    access_key = "AKIAZ24ISOK6LAFT324F"
-    secret_key = "SECRET_KEY"
-    region     = "us-east-1"
-}
-
