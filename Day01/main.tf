e370b0a2fe8d6bf6e60b2b08fa9798f83801f593
Boopalan.G <thirubalan3599@gmail.com>
2025-06-04 03:41:42 +0530

    Update main.tf

--- Day01/main.tf
+++ Day01/main.tf
@@ -2,8 +2,7 @@
 
 # Configure the AWS Provider
 provider "aws" {
-    version = "3.53.0"
-    access_key = "AKIAXC3FBB2OJJC3DUIP"
+    access_key = "AKIAZ24ISOK6LAFT324F"
     secret_key = "SECRET_KEY"
     region     = "us-east-2"
 }
