817c32b256edb6c71fe1c30aa66775265aa2b5c1
Boopalan.G <thirubalan3599@gmail.com>
2025-06-04 03:31:22 +0530

    Update Createinstance.tf

--- Day01/Createinstance.tf
+++ Day01/Createinstance.tf
@@ -2,8 +2,5 @@
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-0731becbf832f281e"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "demoinstance-${count.index}"
-  }
 }
+ 
